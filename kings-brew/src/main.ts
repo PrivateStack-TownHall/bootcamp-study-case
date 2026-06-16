1d0ef93f6635b7438a64b692c3ed4404c84a57b9
vincent-guizot <vincentguizot128@gmail.com>
2026-06-17 06:01:38 +0700

    adding common - filters, interceptors, interfaces. restructuring entities and dtos. update main.ts

--- kings-brew/src/main.ts
+++ kings-brew/src/main.ts
@@ -1,10 +1,21 @@
-import { ValidationPipe } from '@nestjs/common';
+import {
+  ValidationPipe,
+} from '@nestjs/common';
+
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app.module';
 
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+import { ResponseInterceptor } from './common/interceptors/response.interceptor';
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app =
+    await NestFactory.create(
+      AppModule,
+    );
+
+  app.enableCors();
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -14,10 +25,20 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalFilters(
+    new HttpExceptionFilter(),
+  );
+
+  app.useGlobalInterceptors(
+    new ResponseInterceptor(),
+  );
+
+  await app.listen(
+    process.env.PORT ?? 3000,
+  );
 
   console.log(
-    `🚀 Kings Brew API running on http://localhost:${process.env.PORT ?? 3000}`,
+    `🚀 Server running on port ${process.env.PORT ?? 3000}`,
   );
 }
 
