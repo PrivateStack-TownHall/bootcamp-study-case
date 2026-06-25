e81d517897b546f4199b42a973cdcaf2458abe8a
vincent-guizot <vincentguizot128@gmail.com>
2026-06-26 01:43:55 +0700

    observability and public endpoints kings-brew

--- kings-brew/src/app.module.ts
+++ kings-brew/src/app.module.ts
@@ -1,25 +1,36 @@
 import { Module } from '@nestjs/common';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
 import { PrismaModule } from './prisma/prisma.module';
-import { UsersModule } from './users/users.module';
+
 import { AuthModule } from './auth/auth.module';
-import { CategoriesModule } from './categories/categories.module';
-import { ProductsModule } from './products/products.module';
-import { ProductImagesModule } from './product-images/product-images.module';
-import { FavoritesModule } from './favorites/favorites.module';
-import { ReviewsModule } from './reviews/reviews.module';
+import { AuditLogsModule } from './audit-logs/audit-logs.module';
 import { CartModule } from './cart/cart.module';
+import { CategoriesModule } from './categories/categories.module';
+import { FavoritesModule } from './favorites/favorites.module';
+import { OrderStatusHistoryModule } from './order-status-history/order-status-history.module';
 import { OrdersModule } from './orders/orders.module';
 import { PaymentsModule } from './payments/payments.module';
-import { OrderStatusHistoryModule } from './order-status-history/order-status-history.module'
-import { AuditLogsModule } from './audit-logs/audit-logs.module'
+import { ProductImagesModule } from './product-images/product-images.module';
+import { ProductsModule } from './products/products.module';
+import { ReviewsModule } from './reviews/reviews.module';
+import { UsersModule } from './users/users.module';
+
+import { ActivitiesModule } from './observability/activities/activities.module';
+import { HealthModule } from './observability/health/health.module';
+import { MonitoringModule } from './observability/monitoring/monitoring.module';
+import { ResourcesModule } from './observability/resources/resources.module';
+import { StatsModule } from './observability/stats/stats.module';
 
 @Module({
   imports: [
     PrismaModule,
+
     UsersModule,
     AuthModule,
+
     CategoriesModule,
     ProductsModule,
     ProductImagesModule,
@@ -29,9 +40,15 @@ import { AuditLogsModule } from './audit-logs/audit-logs.module'
     OrdersModule,
     PaymentsModule,
     OrderStatusHistoryModule,
-    AuditLogsModule
+    AuditLogsModule,
+
+    HealthModule,
+    StatsModule,
+    ActivitiesModule,
+    MonitoringModule,
+    ResourcesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
