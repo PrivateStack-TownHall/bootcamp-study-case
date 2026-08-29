a00ca0915fff0c44ef9434a780babcaa11b9d62a
vincent-guizot <vincentguizot128@gmail.com>
2026-08-29 22:30:27 +0700

    TH Product Images

--- trade-hub/src/product-images/dto/create-product-image.dto.ts
+++ trade-hub/src/product-images/dto/create-product-image.dto.ts
@@ -1,27 +1,23 @@
 import { ApiProperty } from '@nestjs/swagger';
 
-import {
-   IsInt,
-   IsString,
-} from 'class-validator';
+import { IsInt, IsString } from 'class-validator';
 
 export class CreateProductImageDto {
-   @ApiProperty({
-      example: 30,
-   })
-   @IsInt()
-   productId!: number;
+  @ApiProperty({
+    example: 30,
+  })
+  @IsInt()
+  productId!: number;
 
-   @ApiProperty({
-      example:
-         'https://images.unsplash.com/photo-1544025162-d76694265947',
-   })
-   @IsString()
-   imageUrl!: string;
+  @ApiProperty({
+    example: 'https://images.unsplash.com/photo-1544025162-d76694265947',
+  })
+  @IsString()
+  imageUrl!: string;
 
-   @ApiProperty({
-      example: 1,
-   })
-   @IsInt()
-   sortOrder!: number;
+  @ApiProperty({
+    example: 1,
+  })
+  @IsInt()
+  sortOrder!: number;
 }
