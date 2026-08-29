11b2315affd75f46558e3244297f17991f453921
vincent-guizot <vincentguizot128@gmail.com>
2026-08-29 22:56:03 +0700

    BB Reviews

--- byte-burger/src/reviews/dto/update-review.dto.ts
+++ byte-burger/src/reviews/dto/update-review.dto.ts
@@ -2,32 +2,25 @@
 
 import { ApiPropertyOptional } from '@nestjs/swagger';
 
-import {
-   IsInt,
-   IsOptional,
-   IsString,
-   Max,
-   Min,
-} from 'class-validator';
+import { IsInt, IsOptional, IsString, Max, Min } from 'class-validator';
 
 export class UpdateReviewDto {
-   @ApiPropertyOptional({
-      example: 4,
-      minimum: 1,
-      maximum: 5,
-      description: 'Updated burger rating',
-   })
-   @IsOptional()
-   @IsInt()
-   @Min(1)
-   @Max(5)
-   rating?: number;
+  @ApiPropertyOptional({
+    example: 4,
+    minimum: 1,
+    maximum: 5,
+    description: 'Updated burger rating',
+  })
+  @IsOptional()
+  @IsInt()
+  @Min(1)
+  @Max(5)
+  rating?: number;
 
-   @ApiPropertyOptional({
-      example:
-         'Great burger, updated review after second order',
-   })
-   @IsOptional()
-   @IsString()
-   comment?: string;
+  @ApiPropertyOptional({
+    example: 'Great burger, updated review after second order',
+  })
+  @IsOptional()
+  @IsString()
+  comment?: string;
 }
