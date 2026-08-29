b851a161b59ec1cf77f7dcb9ec928b10f5e37e84
vincent-guizot <vincentguizot128@gmail.com>
2026-08-29 22:51:06 +0700

    BB Prisma

--- byte-burger/prisma/seeds/categories.seed.ts
+++ byte-burger/prisma/seeds/categories.seed.ts
@@ -2,32 +2,30 @@
 
 import { AppType } from '@prisma/client';
 
-export async function seedCategories(
-   prisma: any,
-) {
-   await prisma.category.createMany({
-      data: [
-         {
-            appType: AppType.BURGER,
-            name: 'Beef Burger',
-         },
-         {
-            appType: AppType.BURGER,
-            name: 'Chicken Burger',
-         },
-         {
-            appType: AppType.BURGER,
-            name: 'Cheese Burger',
-         },
-         {
-            appType: AppType.BURGER,
-            name: 'BBQ Burger',
-         },
-         {
-            appType: AppType.BURGER,
-            name: 'Signature Burger',
-         },
-      ],
-      skipDuplicates: true,
-   });
+export async function seedCategories(prisma: any) {
+  await prisma.category.createMany({
+    data: [
+      {
+        appType: AppType.BURGER,
+        name: 'Beef Burger',
+      },
+      {
+        appType: AppType.BURGER,
+        name: 'Chicken Burger',
+      },
+      {
+        appType: AppType.BURGER,
+        name: 'Cheese Burger',
+      },
+      {
+        appType: AppType.BURGER,
+        name: 'BBQ Burger',
+      },
+      {
+        appType: AppType.BURGER,
+        name: 'Signature Burger',
+      },
+    ],
+    skipDuplicates: true,
+  });
 }
