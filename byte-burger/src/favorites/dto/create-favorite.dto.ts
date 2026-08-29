8bed99fa3258d02c634e8381a776ccbfc49465ba
vincent-guizot <vincentguizot128@gmail.com>
2026-08-29 22:54:37 +0700

    BB Favorites

--- byte-burger/src/favorites/dto/create-favorite.dto.ts
+++ byte-burger/src/favorites/dto/create-favorite.dto.ts
@@ -3,10 +3,10 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsInt } from 'class-validator';
 
 export class CreateFavoriteDto {
-   @ApiProperty({
-      example: 1,
-      description: 'Burger ID',
-   })
-   @IsInt()
-   productId!: number;
+  @ApiProperty({
+    example: 1,
+    description: 'Burger ID',
+  })
+  @IsInt()
+  productId!: number;
 }
