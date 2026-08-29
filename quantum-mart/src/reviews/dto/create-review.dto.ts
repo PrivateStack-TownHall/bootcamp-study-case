22ccb5902d3efe2cc37449c0a158af76c0d2dde6
vincent-guizot <vincentguizot128@gmail.com>
2026-08-29 23:13:06 +0700

    QM Reviews

--- quantum-mart/src/reviews/dto/create-review.dto.ts
+++ quantum-mart/src/reviews/dto/create-review.dto.ts
@@ -1,44 +1,31 @@
-import {
-   ApiProperty,
-   ApiPropertyOptional,
-} from '@nestjs/swagger';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
-import {
-   IsInt,
-   IsOptional,
-   IsString,
-   Max,
-   Min,
-} from 'class-validator';
+import { IsInt, IsOptional, IsString, Max, Min } from 'class-validator';
 
 export class CreateReviewDto {
-   @ApiProperty({
-      example: 95,
-      description:
-         'Inventory product id',
-   })
-   @IsInt()
-   productId!: number;
+  @ApiProperty({
+    example: 95,
+    description: 'Inventory product id',
+  })
+  @IsInt()
+  productId!: number;
 
-   @ApiProperty({
-      example: 5,
-      minimum: 1,
-      maximum: 5,
-      description:
-         'Review rating',
-   })
-   @IsInt()
-   @Min(1)
-   @Max(5)
-   rating!: number;
+  @ApiProperty({
+    example: 5,
+    minimum: 1,
+    maximum: 5,
+    description: 'Review rating',
+  })
+  @IsInt()
+  @Min(1)
+  @Max(5)
+  rating!: number;
 
-   @ApiPropertyOptional({
-      example:
-         'Excellent AI assistant with impressive features.',
-      description:
-         'Review comment',
-   })
-   @IsOptional()
-   @IsString()
-   comment?: string;
+  @ApiPropertyOptional({
+    example: 'Excellent AI assistant with impressive features.',
+    description: 'Review comment',
+  })
+  @IsOptional()
+  @IsString()
+  comment?: string;
 }
