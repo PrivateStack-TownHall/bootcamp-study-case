da790f21c3f4a7771ba957efb1ee851c4cc9518c
vincent-guizot <vincentguizot128@gmail.com>
2026-08-29 23:12:49 +0700

    QM Product Images

--- quantum-mart/src/product-images/dto/create-product-image.dto.ts
+++ quantum-mart/src/product-images/dto/create-product-image.dto.ts
@@ -1,38 +1,27 @@
-import {
-   ApiProperty,
-   ApiPropertyOptional,
-} from '@nestjs/swagger';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
-import {
-   IsInt,
-   IsOptional,
-   IsString,
-} from 'class-validator';
+import { IsInt, IsOptional, IsString } from 'class-validator';
 
 export class CreateProductImageDto {
-   @ApiProperty({
-      example: 95,
-      description:
-         'Inventory product id',
-   })
-   @IsInt()
-   productId!: number;
+  @ApiProperty({
+    example: 95,
+    description: 'Inventory product id',
+  })
+  @IsInt()
+  productId!: number;
 
-   @ApiProperty({
-      example:
-         'https://images.unsplash.com/photo-1677442136019-21780ecad995',
-      description:
-         'Product image URL',
-   })
-   @IsString()
-   imageUrl!: string;
+  @ApiProperty({
+    example: 'https://images.unsplash.com/photo-1677442136019-21780ecad995',
+    description: 'Product image URL',
+  })
+  @IsString()
+  imageUrl!: string;
 
-   @ApiPropertyOptional({
-      example: 1,
-      description:
-         'Image display order',
-   })
-   @IsOptional()
-   @IsInt()
-   sortOrder?: number;
+  @ApiPropertyOptional({
+    example: 1,
+    description: 'Image display order',
+  })
+  @IsOptional()
+  @IsInt()
+  sortOrder?: number;
 }
--- quantum-mart/src/product-images/dto/update-product-image.dto.ts
+++ quantum-mart/src/product-images/dto/update-product-image.dto.ts
@@ -1,30 +1,21 @@
-import {
-   ApiPropertyOptional,
-} from '@nestjs/swagger';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 
-import {
-   IsInt,
-   IsOptional,
-   IsString,
-} from 'class-validator';
+import { IsInt, IsOptional, IsString } from 'class-validator';
 
 export class UpdateProductImageDto {
-   @ApiPropertyOptional({
-      example:
-         'https://images.unsplash.com/photo-1677442136019-21780ecad995',
-      description:
-         'Product image URL',
-   })
-   @IsOptional()
-   @IsString()
-   imageUrl?: string;
+  @ApiPropertyOptional({
+    example: 'https://images.unsplash.com/photo-1677442136019-21780ecad995',
+    description: 'Product image URL',
+  })
+  @IsOptional()
+  @IsString()
+  imageUrl?: string;
 
-   @ApiPropertyOptional({
-      example: 2,
-      description:
-         'Image display order',
-   })
-   @IsOptional()
-   @IsInt()
-   sortOrder?: number;
+  @ApiPropertyOptional({
+    example: 2,
+    description: 'Image display order',
+  })
+  @IsOptional()
+  @IsInt()
+  sortOrder?: number;
 }
