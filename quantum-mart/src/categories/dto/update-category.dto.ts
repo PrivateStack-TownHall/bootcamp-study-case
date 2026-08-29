86b9c6bb3e9ec38ac7a5ec9f2309eeafb9b497ff
vincent-guizot <vincentguizot128@gmail.com>
2026-08-29 23:10:54 +0700

    QM Categories

--- quantum-mart/src/categories/dto/update-category.dto.ts
+++ quantum-mart/src/categories/dto/update-category.dto.ts
@@ -1,29 +1,21 @@
-import {
-   IsOptional,
-   IsString,
-} from 'class-validator';
+import { IsOptional, IsString } from 'class-validator';
 
-import {
-   ApiPropertyOptional,
-} from '@nestjs/swagger';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 
 export class UpdateCategoryDto {
-   @ApiPropertyOptional({
-      example: 'Smart Wearables',
-      description:
-         'Inventory category name',
-   })
-   @IsOptional()
-   @IsString()
-   name?: string;
+  @ApiPropertyOptional({
+    example: 'Smart Wearables',
+    description: 'Inventory category name',
+  })
+  @IsOptional()
+  @IsString()
+  name?: string;
 
-   @ApiPropertyOptional({
-      example:
-         'Smart wearable technology products',
-      description:
-         'Inventory category description',
-   })
-   @IsOptional()
-   @IsString()
-   description?: string;
+  @ApiPropertyOptional({
+    example: 'Smart wearable technology products',
+    description: 'Inventory category description',
+  })
+  @IsOptional()
+  @IsString()
+  description?: string;
 }
