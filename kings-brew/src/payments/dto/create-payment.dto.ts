6655d2f344aff3712abbb2647fba6b2c0c531f8a
vincent-guizot <vincentguizot128@gmail.com>
2026-08-29 17:25:31 +0700

    KB Payments

--- kings-brew/src/payments/dto/create-payment.dto.ts
+++ kings-brew/src/payments/dto/create-payment.dto.ts
@@ -1,23 +1,20 @@
 import { ApiProperty } from '@nestjs/swagger';
 
-import {
-   IsEnum,
-   IsInt,
-} from 'class-validator';
+import { IsEnum, IsInt } from 'class-validator';
 
 import { PaymentMethod } from '@prisma/client';
 
 export class CreatePaymentDto {
-   @ApiProperty({
-      example: 1,
-   })
-   @IsInt()
-   orderId!: number;
+  @ApiProperty({
+    example: 1,
+  })
+  @IsInt()
+  orderId!: number;
 
-   @ApiProperty({
-      enum: PaymentMethod,
-      example: PaymentMethod.BANK_TRANSFER,
-   })
-   @IsEnum(PaymentMethod)
-   method!: PaymentMethod;
+  @ApiProperty({
+    enum: PaymentMethod,
+    example: PaymentMethod.BANK_TRANSFER,
+  })
+  @IsEnum(PaymentMethod)
+  method!: PaymentMethod;
 }
