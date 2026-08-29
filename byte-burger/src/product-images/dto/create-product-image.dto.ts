5071f6358ce4a49111d3fe3ab82fa63f6049aef6
vincent-guizot <vincentguizot128@gmail.com>
2026-08-29 22:55:41 +0700

    BB Product Images

--- byte-burger/src/product-images/dto/create-product-image.dto.ts
+++ byte-burger/src/product-images/dto/create-product-image.dto.ts
@@ -1,32 +1,26 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 
-import {
-   IsInt,
-   IsOptional,
-   IsString,
-} from 'class-validator';
+import { IsInt, IsOptional, IsString } from 'class-validator';
 
 export class CreateProductImageDto {
-   @ApiProperty({
-      example: 1,
-      description: 'Burger ID',
-   })
-   @IsInt()
-   productId!: number;
+  @ApiProperty({
+    example: 1,
+    description: 'Burger ID',
+  })
+  @IsInt()
+  productId!: number;
 
-   @ApiProperty({
-      example:
-         'https://images.byteburger.com/double-cheese-burger.jpg',
-   })
-   @IsString()
-   imageUrl!: string;
+  @ApiProperty({
+    example: 'https://images.byteburger.com/double-cheese-burger.jpg',
+  })
+  @IsString()
+  imageUrl!: string;
 
-   @ApiPropertyOptional({
-      example: 1,
-      description:
-         'Display order of burger image',
-   })
-   @IsOptional()
-   @IsInt()
-   sortOrder?: number;
+  @ApiPropertyOptional({
+    example: 1,
+    description: 'Display order of burger image',
+  })
+  @IsOptional()
+  @IsInt()
+  sortOrder?: number;
 }
