8a02962a43e1d03bae2ae2b6e78d7ea5f4eec6e2
vincent-guizot <vincentguizot128@gmail.com>
2026-08-29 18:03:03 +0700

    CK Auth

--- castle-kitchen/src/auth/strategies/jwt.strategy.ts
+++ castle-kitchen/src/auth/strategies/jwt.strategy.ts
@@ -6,35 +6,25 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { UsersService } from '../../users/users.service';
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(
-   Strategy,
-) {
-   constructor(
-      private readonly usersService: UsersService,
-   ) {
-      super({
-         jwtFromRequest:
-            ExtractJwt.fromAuthHeaderAsBearerToken(),
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly usersService: UsersService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
 
-         ignoreExpiration: false,
+      ignoreExpiration: false,
 
-         secretOrKey:
-            process.env.JWT_SECRET ||
-            'supersecret',
-      });
-   }
+      secretOrKey: process.env.JWT_SECRET || 'supersecret',
+    });
+  }
 
-   async validate(payload: any) {
-      const user =
-         await this.usersService.findByEmail(
-            payload.email,
-         );
+  async validate(payload: any) {
+    const user = await this.usersService.findByEmail(payload.email);
 
-      return {
-         id: user?.id,
-         email: user?.email,
-         fullName: user?.fullName,
-         role: user?.role,
-      };
-   }
+    return {
+      id: user?.id,
+      email: user?.email,
+      fullName: user?.fullName,
+      role: user?.role,
+    };
+  }
 }
