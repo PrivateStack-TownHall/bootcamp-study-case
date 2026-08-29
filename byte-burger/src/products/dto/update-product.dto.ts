748b28a4af6b8bda8610ae5e05ba57bae4e236cc
vincent-guizot <vincentguizot128@gmail.com>
2026-08-29 22:55:49 +0700

    BB Products

--- byte-burger/src/products/dto/update-product.dto.ts
+++ byte-burger/src/products/dto/update-product.dto.ts
@@ -1,67 +1,64 @@
 // update-product.dto.ts
 
-import {
-   ApiPropertyOptional,
-} from '@nestjs/swagger';
+import { ApiPropertyOptional } from '@nestjs/swagger';
 
 import { Type } from 'class-transformer';
 
 import {
-   IsBoolean,
-   IsInt,
-   IsNumber,
-   IsOptional,
-   IsString,
-   Min,
+  IsBoolean,
+  IsInt,
+  IsNumber,
+  IsOptional,
+  IsString,
+  Min,
 } from 'class-validator';
 
 export class UpdateProductDto {
-   @ApiPropertyOptional({
-      example: 1,
-      description: 'Burger category id',
-   })
-   @IsOptional()
-   @Type(() => Number)
-   @IsInt()
-   categoryId?: number;
+  @ApiPropertyOptional({
+    example: 1,
+    description: 'Burger category id',
+  })
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  categoryId?: number;
 
-   @ApiPropertyOptional({
-      example: 'Double Cheese Burger',
-   })
-   @IsOptional()
-   @IsString()
-   name?: string;
+  @ApiPropertyOptional({
+    example: 'Double Cheese Burger',
+  })
+  @IsOptional()
+  @IsString()
+  name?: string;
 
-   @ApiPropertyOptional({
-      example:
-         'Juicy beef burger with double cheddar cheese',
-   })
-   @IsOptional()
-   @IsString()
-   description?: string;
+  @ApiPropertyOptional({
+    example: 'Juicy beef burger with double cheddar cheese',
+  })
+  @IsOptional()
+  @IsString()
+  description?: string;
 
-   @ApiPropertyOptional({
-      example: 55000,
-   })
-   @IsOptional()
-   @Type(() => Number)
-   @IsNumber()
-   @Min(0)
-   price?: number;
+  @ApiPropertyOptional({
+    example: 55000,
+  })
+  @IsOptional()
+  @Type(() => Number)
+  @IsNumber()
+  @Min(0)
+  price?: number;
 
-   @ApiPropertyOptional({
-      example: 100,
-   })
-   @IsOptional()
-   @Type(() => Number)
-   @IsInt()
-   @Min(0)
-   stock?: number;
+  @ApiPropertyOptional({
+    example: 100,
+  })
+  @IsOptional()
+  @Type(() => Number)
+  @IsInt()
+  @Min(0)
+  stock?: number;
 
-   @ApiPropertyOptional({
-      example: true,
-   })
-   @IsOptional()
-   @IsBoolean()
-   isActive?: boolean;
+  @ApiPropertyOptional({
+    example: true,
+  })
+  @IsOptional()
+  @IsBoolean()
+  isActive?: boolean;
 }
